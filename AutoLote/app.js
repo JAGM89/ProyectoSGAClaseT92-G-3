c39cd82d067220609d3720a86236651094c6dec3
Brayan Argenal <argenalbrian4@gmail.com>
2025-03-26 16:06:05 -0600

    Update usuario.js
    Add usuario.js

--- AutoLote/app.js
+++ AutoLote/app.js
@@ -8,17 +8,16 @@ const PORT=process.env.PORT;
 const vehiculoRoutes = require('./routes/vehiculos');
 const clientesRoutes = require('./routes/clientes');
 const ventasRouter = require('./routes/ventas');
+const usuariosRoutes = require('./routes/usuarios');
 
 
 
-
+app.use('/api', usuariosRoutes);
 //const ventaRoutes=require('./routes/ventas');
 app.use(express.json());
 app.use('/api',vehiculoRoutes);
 app.use('/api',clientesRoutes); // Api de clientes
 app.use('/api', ventasRouter);
-//app.use('/api',ventaRoutes);
-
 
 
 app.listen(PORT, ()=>{
